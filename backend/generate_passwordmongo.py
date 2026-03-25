9deac5e12c7cbe9bf2383c9d06dde74beb843430
Astha <146747238+asthatripathi434@users.noreply.github.com>
2026-03-25 19:36:55 +0530

    Update generate_passwordmongo.py

--- backend/generate_passwordmongo.py
+++ backend/generate_passwordmongo.py
@@ -1,7 +1,7 @@
 from app.core.security import hash_password
 
 def main():
-    plain_password = "MyStrongPass@2026"   # choose a strong password
+    plain_password = "#########"   # choose a strong password
     hashed = hash_password(plain_password)
     print(f"Plain: {plain_password}")
     print(f"Hashed: {hashed}")
